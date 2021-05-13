9c6f213dd63cf281504937ac6723d6b0cd153d1e
hamzsait <hamzasait@utexas.edu>
2021-05-12 22:59:01 -0500

    added index.js for route structre

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router()
+const path = require("path")
+router.use("/", require("./homeRoutes.js"))
+//router.use("/api", require("./api"))
+
+module.exports= router 
