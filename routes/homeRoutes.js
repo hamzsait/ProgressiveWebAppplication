daa19cd5bffd5edac0daec0feb68aac9106e8ba4
hamzsait <hamzasait@utexas.edu>
2021-05-12 22:58:25 -0500

    added all routes needed for front end code

---
+++ routes/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const path = require('path')
+
+// sendFile will go here
+router.get('/', function(req, res) {
+
+    try{
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    }
+
+    catch{
+        res.json({error})
+    }
+
+});
+
+router.get('/stats', async (req, res) => {
+    
+    try{
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
+    }
+
+    catch{
+        res.json({error})
+    }
+
+})
+
+module.exports=router
