0ae676fa0e21bca1e8cdb41936acfb36ea24f9d8
hamzsait <hamzasait@utexas.edu>
2021-05-12 23:53:01 -0500

    completed model of workout

--- models/Workout.js
+++ models/Workout.js
@@ -4,5 +4,41 @@ const Schema = mongoose.Schema
 
 const workOut = new Schema({
 
-    
+    day:{
+        type:Date,
+        default: new Date()
+    },
+    exercises: [
+        {
+            type:{
+                type:String,
+                required:"Please enter type of workout"
+            },
+            name: {
+                type:String,
+                required:"Please enter name of exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter duration of workout"
+            },
+            weight:{
+                type:Number,
+                required: "Please enter weight of workout"
+            },
+            reps:{
+                type:Number,
+                required: "Please enter number of reps performed"
+            },
+            sets:{
+                type:Number,
+                required:"Please enter number of sets performed"
+            }
+        }
+    ]
+
 })
+
+const Workout = mongoose.model("Workout", workOut);
+
+module.exports = Workout
