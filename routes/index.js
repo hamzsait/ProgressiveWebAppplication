209f7d2cefa0ba5d95c16a314ea766d09e1935f5
hamzsait <hamzasait@utexas.edu>
2021-05-14 22:51:27 -0500

    refactord index of routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
-const path = require("path")
+
 router.use("/", require("./homeRoutes.js"))
 router.use("/api", require("./api"))
 
-module.exports= router 
+module.exports = router 
